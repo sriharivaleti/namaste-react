4ee016c37d17fe354b40f5bdd064438499f75df0
Valeti, Sreehari <sreehari.valeti@sscinc.com>
2024-04-23 18:26:16 +0530

    updated with parcel bundler

--- App.js
+++ App.js
@@ -9,11 +9,13 @@
  * 
  * 
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement('div',
 {id:'parent'},
 [React.createElement('div',
 {id:'child'},
-[React.createElement('h1',{},'im h1 tag'),React.createElement("h2",{},"im h2 tag"),]),
+[React.createElement('h1',{},'im h1 tag'),React.createElement("h2",{},"This is Namate React "),]),
 React.createElement('div',
 {id:'child2'},
 [React.createElement('h1',{},'im h1 tag'),React.createElement("h2",{},"im h2 tag"),])]);
